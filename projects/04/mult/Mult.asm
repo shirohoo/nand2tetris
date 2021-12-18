28cf700df139210b0cd9a1ae871430d235dbca31
shirohoo <shirohoo4102@gmail.com>
2021-12-18 14:00:41 +0900

    :sparkles: Mult.hack을 구현했다

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,34 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Put your code here
+@R2
+M=0 // int R2 = 0;
+
+@i
+M=0 // int i = 0;
+
+(LOOP)
+    @i
+    D=M // D = i;
+
+    @R0
+    D=D-M // if((D - R0) == 0) then end the loop else multiply
+
+    @END
+    D;JEQ // loop end
+
+    @R1
+    D=M // D = R1;
+
+    @R2
+    M=D+M // R2 += D;
+
+    @i
+    M=M+1 // i++;
+
+    @LOOP
+    0;JMP // loop continue
+(END)
+    @END
+    0;JMP // terminate
