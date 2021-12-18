dbc112af9fb17147fb60395e9a3a53e8724b430a
shirohoo <shirohoo4102@gmail.com>
2021-12-18 15:24:14 +0900

    :sparkles: Fill.hack을 구현했다

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,29 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(INPUTCHECK)
+    @KBD
+    D=M // 키보드 입력을 받는다. 입력이 없다면 D==0
+
+    @BLACK 
+    D;JGT // 키보드 입력 값이 0보다 크면 BLACK으로 점프
+
+    @WHITE 
+    D;JEQ // 키보드 입력 값이 0이면 WHITE로 점프
+
+    @INPUTCHECK
+    0;JMP // 이도저도 아니면 다시 처음으로
+
+(BLACK)
+    @SCREEN
+    M=-1 // 화면을 검은색으로 칠한다
+
+    @INPUTCHECK
+    0;JMP // 다시 INPUTCHECK로 점프 
+
+(WHITE)
+    @SCREEN
+    M=0 // 화면을 하얀색으로 칠한다
+
+    @INPUTCHECK
+    0;JMP // 다시 INPUTCHECK로 점프 
